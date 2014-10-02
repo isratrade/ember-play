cb897829c26ac3764265ed533df2ae3fd910604b
Joseph Magen <joseph@isratrade.co.il>
2014-10-02 16:58:31 +0300

    added route - hostgroup

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('hostgroups', function() {
+    this.resource('hostgroup', { path: '/:hostgroup_id' });
+  });
 });
 
 export default Router;

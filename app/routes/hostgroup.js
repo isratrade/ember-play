be4c81a6fb1c84989b0e325923dc4b0e1d2924a2
Joseph Magen <joseph@isratrade.co.il>
2014-10-02 20:58:37 +0300

    added route - hostgroup

---
+++ app/routes/hostgroup.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('hostgroup', params.hostgroup_id)
+  },
+
+  activate:   function() {
+    //this.controllerFor('hostgroup').set('showColumns', false)
+    console.log('entered hostgroup route');
+  },
+
+  deactivate:   function() {
+    //this.controllerFor('hostgroup').set('showColumns', false)
+    console.log('left hostgroup route');
+  }
+});
+

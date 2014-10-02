d5d13ba244b327c73aa4e08cfc180bf925bf23c6
Joseph Magen <joseph@isratrade.co.il>
2014-10-02 20:14:16 +0300

    subnet model

---
+++ app/models/subnet.js
@@ -0,0 +1,17 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  network: DS.attr('string'),
+  cidr: DS.attr('string'),
+  mask: DS.attr('string'),
+  priority: DS.attr('string'),
+  vlanid: DS.attr('string'),
+  gateway: DS.attr('string'),
+  dns_primary: DS.attr('string'),
+  dns_secondary: DS.attr('string'),
+  from: DS.attr('string'),
+  to: DS.attr('string'),
+  created_at: DS.attr('date'),
+  updated_at: DS.attr('date')
+});

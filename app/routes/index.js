3b0c7d2c4810e5c75abfbc934f52cc6e30fee9a8
Joseph Magen <joseph@isratrade.co.il>
2014-10-05 16:50:01 +0300

    reverted to ember-cli-bootstrap, not SASS

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel: function( transition ) {
     // redirect root to hostgroups
-    this.transitionTo( 'subnets' );
+    this.transitionTo('subnets');
   }
 });

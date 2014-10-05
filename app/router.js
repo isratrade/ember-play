c5586dfa2748f3c2fb47112dc652f0abd9318602
Joseph Magen <joseph@isratrade.co.il>
2014-10-05 14:04:01 +0300

    added Index Route

--- app/router.js
+++ app/router.js
@@ -2,7 +2,11 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  // log when Ember generates a controller or a route from a generic class
+  LOG_ACTIVE_GENERATION: true,
+  // log when Ember looks up a template or a view
+  LOG_VIEW_LOOKUPS: true
 });
 
 Router.map(function() {
---
+++ app/routes/index.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  beforeModel: function( transition ) {
+    // redirect root to hostgroups
+    this.transitionTo( 'hostgroups' );
+  }
+});

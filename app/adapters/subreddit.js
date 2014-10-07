1239830ef8456b25ae5ccc3a7fa12867a59fc585
Joseph Magen <joseph@isratrade.co.il>
2014-10-07 17:16:30 +0300

    subreddit refactoring using adapter

--- app/adapters/subreddit.js
+++ app/adapters/subreddit.js
@@ -5,6 +5,14 @@ export default Ember.Object.extend({
   find: function(name, id) {
     // subreddit
     // aww
-    return ajax("http://www.reddit.com/r/" + id + ".json");
+    return ajax("http://www.reddit.com/r/" + id + ".json")
+      .then(function(result) {
+        return result.data.children.map(function(c) {
+          return {id: c.id,
+                  title: c.data.title,
+                  domain: c.data.domain,
+                  url: c.data.url};
+        });
+      });
   }
 });

37bde1a4785c19abac6f54574c834a11189e70a8
Joseph Magen <joseph@isratrade.co.il>
2014-10-13 16:54:59 +0300

    users searching

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -4,8 +4,25 @@ export default Ember.ArrayController.extend({
   sortProperties: ['id'],
   sortAscending: false,
 
-  cntUsers: function() {
+  cntAllUsers: function() {
       return this.get('length');
-  }.property('@each')
+  }.property('@each'),
+
+  searchedUsers: function() {
+    var search = this.get('search').toLowerCase();
+    //return this.filterProperty('firstname', search);  // this searches ONLY by first name Joe, not partial, Jo
+    return this.filter(function(user) {
+      return (user.get('fullName').toLowerCase().indexOf(search) !== -1);
+    });
+  }.property('search', 'this.@each'),
+
+  filteredUsers: function() {
+    return this.get('search') ? this.get('searchedUsers') : this;
+  }.property('search', 'searchedUsers'),
+
+  cntFilteredUsers: function() {
+      return this.get('filteredUsers').get('length');
+  }.property('search', 'this.@each')
+
 
 });
